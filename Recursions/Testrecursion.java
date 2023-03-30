2bdcbc22fd9612f4b82fe498a4953e8b833148a4
Santosh-625 <125048218+Santosh-625@users.noreply.github.com>
2023-03-31 00:13:04 +0530

    myfirstproblem
    I learned debugging by using VS code IDE.

---
+++ Recursions/Testrecursion.java
@@ -0,0 +1,14 @@
+package Recursions;
+
+public class Testrecursion {
+    public static void main(String[] args) {
+        int i=0;
+        while(true){
+           
+            i++;
+            if(i==10)
+            break;
+            System.out.println(i);
+        }
+    }
+}
